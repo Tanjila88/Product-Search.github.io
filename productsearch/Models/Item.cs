83055aca992f3506f75239e8c46d7a864495b912
Tanjila88 <disha.ctg88@gmail.com>
2020-01-11 07:46:05 +0600

    Create Item.cs

---
+++ productsearch/Models/Item.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProductSearch.Models
+{
+    public class Item
+    {
+        [Key]
+        public int ItemID { get; set; }
+        public int ProductID { get; set; }
+        public virtual Product Product { get; set; }
+        public decimal ItemPrice { get; set; }
+        public decimal Discount { get; set; }
+        public decimal Vat { get; set; }
+        public bool IsAvailable { get; set; }
+    }
+}
