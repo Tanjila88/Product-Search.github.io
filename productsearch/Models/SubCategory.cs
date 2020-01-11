871c1d1981ddb1481e78dfaf4b19fc6e3f5cfcd4
Tanjila88 <disha.ctg88@gmail.com>
2020-01-11 07:46:57 +0600

    Create SubCategory.cs

---
+++ productsearch/Models/SubCategory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ProductSearch.Models
+{
+    public class SubCategory
+    {
+        [Key]
+        [Display(Name ="Subcategory ID")]
+        public int SubCategoryID { get; set; }
+        [Required]
+        [Display(Name ="Subcategory")]
+        public string SubCategoryName { get; set; }
+        
+        [Display(Name = "Category")]
+        public int CategoryID { get; set; }
+        public virtual Category Category { get; set; }
+        public virtual ICollection<Product> Products { get; set; }
+    }
+}
