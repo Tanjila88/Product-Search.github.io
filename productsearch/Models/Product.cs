69e5ae2df13171e8b472446a3791c8b24e08eefd
Tanjila88 <disha.ctg88@gmail.com>
2020-01-11 07:46:17 +0600

    Create Product.cs

---
+++ productsearch/Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ProductSearch.Models
+{
+    public class Product
+    {
+        [Key]
+        public int ProductID { get; set; }
+        [Display(Name = "Product Name")]
+        public string ProductName { get; set; }
+        [Display(Name = "Measurment Unit")]
+        public string Unit { get; set; }
+        [Display(Name = "Subcategory")]
+        public int SubCategoryID { get; set; }
+        public virtual SubCategory SubCategory { get; set; }
+        public virtual ICollection<Purchase> Purchases { get; set; }
+    }
+}
