ffbbd4c408b9feabba0f63e18de7a55601cd9a9c
Tanjila88 <disha.ctg88@gmail.com>
2020-01-11 07:46:10 +0600

    Create Notification.cs

---
+++ productsearch/Models/Notification.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ProductSearch.Models
+{
+    public class Notification
+    {
+        [Key]
+        public int NotificationID { get; set; }
+        public string NotifyText { get; set; }
+        public string NotifyDate { get; set; }
+    }
+}
