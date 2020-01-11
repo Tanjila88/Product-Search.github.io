d040019d8721b14d7678626246f1d19c8b6feb87
Tanjila88 <disha.ctg88@gmail.com>
2020-01-11 07:46:00 +0600

    Create Category.cs

---
+++ productsearch/Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ProductSearch.Models
+{
+    public class Category
+    {
+        [Key]
+        [Display(Name ="Category ID")]
+        public int CategoryID { get; set; }
+        [Display(Name ="Catagory Name")]
+        [Required]
+        public string CategoryName { get; set; }
+        public virtual ICollection<SubCategory> SubCategories { get; set; }
+    }
+}
