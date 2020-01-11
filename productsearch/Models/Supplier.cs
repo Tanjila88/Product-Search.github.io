9f79810b544b794b0ea5df805cabe3c9cb475853
Tanjila88 <disha.ctg88@gmail.com>
2020-01-11 07:47:03 +0600

    Create Supplier.cs

---
+++ productsearch/Models/Supplier.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ProductSearch.Models
+{
+    public class Supplier
+    {
+        [Key]
+        public int SupplierID { get; set; }
+        public string SupplierName { get; set; }
+        public virtual ICollection<Purchase> Purchases { get; set; }
+
+    }
+}
