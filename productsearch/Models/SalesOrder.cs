18d77b0172778f72c738ade4c51421f8416b962d
Tanjila88 <disha.ctg88@gmail.com>
2020-01-11 07:46:42 +0600

    Create SalesOrder.cs

---
+++ productsearch/Models/SalesOrder.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ProductSearch.Models
+{
+    public class SalesOrder
+    {
+        [Key]
+        public int SalesOrderID { get; set; }
+        public int ItemID { get; set; }
+        public virtual Item Item { get; set; }
+        public decimal OrderQnty { get; set; }
+        public string SalesDate { get; set; }
+    }
+}
