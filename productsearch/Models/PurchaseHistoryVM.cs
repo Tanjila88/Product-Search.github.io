39ec2b53b0067eade4a84187d97c1076be5c5562
Tanjila88 <disha.ctg88@gmail.com>
2020-01-11 07:46:38 +0600

    Create PurchaseHistoryVM.cs

---
+++ productsearch/Models/PurchaseHistoryVM.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProductSearch.Models
+{
+    public class PurchaseHistoryVM
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public string Unit { get; set; }
+        public int CategoryID { get; set; }
+        public string CategoryName { get; set; }
+        public int SubCategoryID { get; set; }
+        public string SubCategoryName { get; set; }
+        public int PurchaseID { get; set; }
+        public decimal PurchaseQuantity { get; set; }
+        public decimal PurchasePrice { get; set; }
+        public string PurchaseDate { get; set; }
+        public string SupplierName { get; set; }
+    }
+}
