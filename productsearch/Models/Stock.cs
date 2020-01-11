a58d5d28ea1b70bb6f44810c754c796319ef39d8
Tanjila88 <disha.ctg88@gmail.com>
2020-01-11 07:46:47 +0600

    Create Stock.cs

---
+++ productsearch/Models/Stock.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ProductSearch.Models
+{
+    public class Stock
+    {
+        [Key]
+        public int StockID { get; set; }
+        public int ProductID { get; set; }
+        public decimal StockQuantity { get; set; }
+    }
+}
