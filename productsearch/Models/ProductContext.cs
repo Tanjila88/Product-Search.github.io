35e9f6b082aa60c2f279769edad2ad8c04a43dbe
Tanjila88 <disha.ctg88@gmail.com>
2020-01-11 07:46:23 +0600

    Create ProductContext.cs

---
+++ productsearch/Models/ProductContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace ProductSearch.Models
+{
+    public class ProductContext : DbContext
+    {
+        public ProductContext():base("DbCon")
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<SubCategory> SubCategories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Purchase> Purchases { get; set; }
+
+        public System.Data.Entity.DbSet<ProductSearch.Models.Supplier> Suppliers { get; set; }
+
+        public System.Data.Entity.DbSet<ProductSearch.Models.Stock> Stocks { get; set; }
+
+        public System.Data.Entity.DbSet<ProductSearch.Models.SalesOrder> SalesOrders { get; set; }
+
+        public System.Data.Entity.DbSet<ProductSearch.Models.Item> Items { get; set; }
+
+        public System.Data.Entity.DbSet<ProductSearch.Models.Notification> Notifications { get; set; }
+    }
+}
